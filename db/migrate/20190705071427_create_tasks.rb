0de91396822be69cf51871799d7c538fa42cdaa2
EC2 Default User <ec2-user@ip-172-31-35-250.ap-northeast-1.compute.internal>
2019-07-31 04:43:57 +0000

    SQLite3-SQLException

--- db/migrate/20190705071427_create_tasks.rb
+++ db/migrate/20190705071427_create_tasks.rb
@@ -1,6 +1,6 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
-    create_table :tasks do |t|
+    create_table :task do |t|
       t.string :name
       t.text :description
       t.references :user, foreign_key: true
