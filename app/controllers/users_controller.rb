801bd009399f532dbcc52807ff207ee3cbc09d72
EC2 Default User <ec2-user@ip-172-31-35-250.ap-northeast-1.compute.internal>
2019-08-05 06:16:18 +0000

    UpdateError

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,10 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(
+        :password,
+        :password_confirmation
+      )
     end
     
     def set_user
