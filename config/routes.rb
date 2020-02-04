9b20acb8fea3d19350aeb968f839b9a604507552
EC2 Default User <ec2-user@ip-172-31-33-217.ap-northeast-1.compute.internal>
2020-02-04 02:50:03 +0000

    success

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  post '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
